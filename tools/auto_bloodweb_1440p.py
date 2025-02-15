958e4e8b645714a083f0e08fb22071120cc2de88
Pazdikan <pazdikan@gmail.com>
2025-02-15 20:07:56 +0100

    Add auto bloodweb tools

--- tools/auto_bloodweb_1440p.py
+++ tools/auto_bloodweb_1440p.py
@@ -10,16 +10,16 @@ reader = easyocr.Reader(['en'])
 
 
 def level_up():
-    pyautogui.mouseDown(x=682, y=563)
+    pyautogui.mouseDown(x=912, y=759)
     sleep(0.1)
-    pyautogui.mouseUp(x=682, y=563)
+    pyautogui.mouseUp(x=912, y=759)
 
 with mss() as sct:
     while True:
         window = win32gui.GetForegroundWindow()
         window_title = win32gui.GetWindowText(window)
         if "DeadByDaylight" in window_title :
-            screenshot = sct.grab({'top': 86, 'left': 592, 'width': 50, 'height': 50})
+            screenshot = sct.grab({'top': 116, 'left': 784, 'width': 75, 'height': 75})
             image = Image.frombytes("RGB", screenshot.size, screenshot.bgra, "raw", "BGRX")
             text = reader.readtext(np.array(image), detail=0)
 
